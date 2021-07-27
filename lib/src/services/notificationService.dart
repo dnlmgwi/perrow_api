27f0b86eaf11829343e7dd831b1ff345237248a5
Daniel P. Mgawi <54575392+sketchdm@users.noreply.github.com>
2021-07-27 11:35:47 +0200

    v0.1.0
    - SMS Formatting

--- lib/src/services/notificationService.dart
+++ lib/src/services/notificationService.dart
@@ -27,66 +27,123 @@ class NotificationService {
   /// Notify the Sender and The Reciver of this transaction
   Future<void> transferNotification(PostgrestResponse value) async {
     /// Notify Sender
-    await paymentProcessed(
+    await senderNotificationSMS(
       id: TransactionRecord.fromJson(value.data[0]).sender,
+      phoneNumber: '',
       message: {
         'message': 'Transfer Complete',
         'recipient': TransactionRecord.fromJson(value.data[0]).recipient,
         'amount': TransactionRecord.fromJson(value.data[0]).amount,
+        'trans_id': TransactionRecord.fromJson(value.data[0]).transId,
+        'timestamp': TransactionRecord.fromJson(value.data[0]).timestamp
       },
     );
 
     /// Nofitfy Recipient
-    await paymentProcessed(
+    await recipientNotificationSMS(
       id: TransactionRecord.fromJson(value.data[0]).recipient,
+      phoneNumber: '',
       message: {
         'message': 'Payment Recieved',
         'sender': TransactionRecord.fromJson(value.data[0]).sender,
         'amount': TransactionRecord.fromJson(value.data[0]).amount,
+        'trans_id': TransactionRecord.fromJson(value.data[0]).transId,
+        'timestamp': TransactionRecord.fromJson(value.data[0]).timestamp
       },
     );
   }
 
-  Future<void> depositNotification(PostgrestResponse value) async {
-    /// Notify User of their deposit
-    await paymentProcessed(
-      id: TransactionRecord.fromJson(value.data[0]).sender,
-      message: {
-        'message': 'Deposit Complete',
-        'amount': TransactionRecord.fromJson(value.data[0])
-            .amount, //TODO Details in Notifcation
-      },
-    );
+  // Future<void> depositNotification(PostgrestResponse value) async {
+  //   /// Notify User of their deposit
+  //   await sendSMS(
+  //     id: TransactionRecord.fromJson(value.data[0]).sender,
+  //     phoneNumber: '',
+  //     message: {
+  //       'message': 'Deposit Complete',
+  //       'amount': TransactionRecord.fromJson(value.data[0])
+  //           .amount, //TODO Details in Notifcation
+  //       'trans_id': TransactionRecord.fromJson(value.data[0]).transId,
+  //       'timestamp': TransactionRecord.fromJson(value.data[0]).timestamp
+  //     },
+  //   );
+  // }
+
+  // Future<void> withdrawNotification(PostgrestResponse value) async {
+  //   /// Notify User of their withdraw
+  //   await sendSMS(
+  //     id: TransactionRecord.fromJson(value.data[0]).sender,
+  //     phoneNumber: '',
+  //     message: {
+  //       'message': 'Withdraw Complete',
+  //       'amount': TransactionRecord.fromJson(value.data[0])
+  //           .amount, //TODO Details in Notification
+  //       'trans_id': TransactionRecord.fromJson(value.data[0]).transId,
+  //       'timestamp': TransactionRecord.fromJson(value.data[0]).timestamp
+  //     },
+  //   );
+  // }
+
+  // /// Base Notification
+  // Future<void> sendSMS(
+  //     {required String id,
+  //     required Map message,
+  //     required String phoneNumber}) async {
+  //   africasTalking.isLive = false;
+
+  //   var sms = africasTalking.sms('67567');
+
+  //   await sms.send(
+  //       message:
+  //           'Dear Customer ${message['message']}, ${message['amount']} from ${message['id']}, on ${DateTimeFormat.format(DateTime.fromMillisecondsSinceEpoch(message['timestamp']), format: 'D, M j, H:i')}, Ref: ${message['trans_id']} ',
+  //       to: ['+265997176756']).onError((error, stackTrace) {
+  //     //TODO Add Phone Number Variable
+  //     print(error);
+  //     throw UnimplementedError(); //TODO Handle Error
+  //   });
+  // }
+
+  Future<void> senderNotificationSMS(
+      {required String id,
+      required Map message,
+      required String phoneNumber}) async {
+    africasTalking.isLive = false;
+
+    var sms = africasTalking.sms('67567');
+
+    await sms.send(
+        message:
+            'Dear customer ${message['message']}, ${message['amount']} to $id on ${dateTimeReadable(message)}. Ref: ${message['trans_id']}',
+        to: ['+265997176756']).onError((error, stackTrace) {
+      //TODO Add Phone Number Variable
+      print(error);
+      throw UnimplementedError(); //TODO Handle Error
+    });
   }
 
-  Future<void> withdrawNotification(PostgrestResponse value) async {
-    /// Notify User of their withdraw
-    await paymentProcessed(
-      id: TransactionRecord.fromJson(value.data[0]).sender,
-      message: {
-        'message': 'Withdraw Complete',
-        'amount': TransactionRecord.fromJson(value.data[0])
-            .amount, //TODO Details in Notification
-      },
-    );
-  }
-
-  /// Base Notification
-  Future<void> paymentProcessed({
+  Future<void> recipientNotificationSMS({
     required String id,
     required Map message,
+    required String phoneNumber,
   }) async {
     africasTalking.isLive = false;
 
     var sms = africasTalking.sms('67567');
 
     await sms.send(
-        message: 'Dear Customer SMS Notification Test, ${message['message']}. ${message['amount']} ',
-        to: ['+********']).onError((error, stackTrace) {
+        message:
+            'Dear customer ${message['message']}, ${message['amount']} from $id on ${dateTimeReadable(message)}. Ref: ${message['trans_id']}',
+        to: ['+265997176756']).onError((error, stackTrace) {
+      //TODO Add Phone Number Variable
       print(error);
       throw UnimplementedError(); //TODO Handle Error
     });
   }
+
+  String dateTimeReadable(Map<dynamic, dynamic> message) =>
+      DateTimeFormat.format(
+        DateTime.fromMillisecondsSinceEpoch(message['timestamp']),
+        format: 'D, M j, H:i',
+      );
 
   static final africasTalking = AfricasTalking('sandbox', Env.africasTalking!);
 }
